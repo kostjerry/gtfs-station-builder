3f32a0f86491f807a06acf42b9e719e0f34a1aff
Jerry <kostjerry@gmail.com>
2019-07-31 15:53:20 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello world!
+      </header>
+    </div>
+  );
+}
+
+export default App;
